8da1bfcb5b3ce760194f924bf1b419b8a3f70601
kalem868 <kalemmalek123@gmail.com>
2023-06-27 19:03:51 -0400

    no

--- src/helper/api.ts
+++ src/helper/api.ts
@@ -1,12 +1,8 @@
 import axios from "axios";
-import https from "https";
 import { QueryClient } from "react-query";
 
 const apiClient = axios.create({
   baseURL: `${process.env.REACT_APP_API_ENDPOINT}/`,
-  httpsAgent: new https.Agent({
-    rejectUnauthorized: false,
-  }),
 });
 
 const queryClient = new QueryClient();
