cdad8564f3e2aa566f6aec428483b320acecb49c
kalem868 <kalemmalek123@gmail.com>
2023-06-28 18:10:21 -0400

    yea

--- src/helper/api.ts
+++ src/helper/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { QueryClient } from "react-query";
 
 const apiClient = axios.create({
-  baseURL: `https://18.222.121.148:5000/api`,
+  baseURL: `${process.env.REACT_APP_API_ENDPOINT}`,
 });
 
 const queryClient = new QueryClient();
